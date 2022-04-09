0de7ad9b0e2c60ec04ddb7b1d03046f8cb3a1c15
Andrew W.Y. Sit <57700483+andrewwys@users.noreply.github.com>
2022-04-09 02:13:08 -0400

    Bug fix. Now able to transform json data

--- group4_flask.py
+++ group4_flask.py
@@ -9,16 +9,17 @@ from flask import Flask, request, jsonify
 import traceback
 import pandas as pd
 #from sklearn import preprocessing
-import pickle
+# import pickle
+import joblib
 import sys
 from os import path
 
 project_folder = r'C:\Projects\COMP247\Final_Project\_deploy'
 models = {
-        "Random Forest": "group4_nn_fullpipe_v7_andrew.pkl",
-        "Neuro Network": "group4_nn_fullpipe_v7_andrew.pkl",
-        "Decision Tree": "group4_nn_fullpipe_v7_andrew.pkl",
-        "Logistic Regression": "group4_nn_fullpipe_v7_andrew.pkl",
+        "Random_Forest": "group4_nn_fullpipe_v7_andrew.pkl",
+        "Neuro_Network": "group4_nn_fullpipe_v7_andrew.pkl",
+        "Decision_Tree": "group4_nn_fullpipe_v7_andrew.pkl",
+        "Logistic_Regression": "group4_nn_fullpipe_v7_andrew.pkl",
         "SVM": "group4_nn_fullpipe_v7_andrew.pkl"
     }
 
@@ -33,17 +34,12 @@ def predict(model_name):
         try:
             json_ = request.json
             print('JSON: \n', json_)
-            query = pd.get_dummies(pd.DataFrame(json_))
-            query = query.reindex(columns=model_columns, fill_value=0)
-            print('Query: \n', query)
-
-            # query = pd.DataFrame(query, columns=model_columns)
-            # print(query)
+            query = pd.DataFrame(json_, columns=model_columns)
+            print(query.info())
             prediction = list(loaded_model[model_name].predict(query))
             print({'prediction': str(prediction)})
             return jsonify({'prediction': str(prediction)})
             return "Welcome to COMP247 - Group 4 APIs!"
-
         except:
             return jsonify({'trace': traceback.format_exc()})
     else:
@@ -59,10 +55,15 @@ if __name__ == '__main__':
     # load all models:
     loaded_model = {}
     for model_name in (models):
-        loaded_model[model_name] = pickle.load(open(path.join(project_folder, models[model_name]),"rb"))
+        loaded_model[model_name] = joblib.load(path.join(project_folder, models[model_name]))
         print(f'Model {model_name} loaded')
         
-    model_columns = pickle.load(open(path.join(project_folder, cols_pkl),"rb"))
-    print ('Model columns loaded')
+    model_columns = ['Elapsed_Days_Before_Reported', 'Primary_Offence', 'Occurrence_Year',
+           'Occurrence_DayOfWeek', 'Occurrence_DayOfYear', 'Occurrence_Hour',
+           'Division', 'City', 'Hood_ID', 'Premises_Type', 'Bike_Make',
+           'Bike_Model', 'Bike_Type']
+    # model_columns = joblib.load(path.join(project_folder, cols_pkl))
+    # print ('Model columns loaded')
     
     app.run(port=port, debug=True)
+    
